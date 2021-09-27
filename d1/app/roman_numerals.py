3cf3115b3b05b668db70e8d96b58a5d07d9496f2
Arnold C <acabang23@gmail.com>
2021-09-27 16:21:40 -0700

    finished thru 8

--- d1/app/roman_numerals.py
+++ d1/app/roman_numerals.py
@@ -1,3 +1,9 @@
 def parse(arg):
     if(arg == "I"):return 1
     elif(arg == "II"):return 2
+    elif(arg == "III"):return 3
+    elif(arg == "IV"):return 4
+    elif(arg == "V"):return 5
+    elif(arg == "VI"):return 6
+    elif(arg == "VII"):return 7
+    elif(arg == "VIII"):return 8
--- d1/test/test_roman_numerals_unittest.py
+++ d1/test/test_roman_numerals_unittest.py
@@ -16,14 +16,22 @@ class TestRomanNumerals():
         value = parse("III")
         assert value == 3
 
-    def test_iii(self):
+    def test_iv(self):
         value = parse("IV")
         assert value == 4
 
-    def test_iii(self):
+    def test_v(self):
         value = parse("V")
         assert value == 5
 
-    def test_iii(self):
+    def test_vi(self):
         value = parse("VI")
         assert value == 6
+
+    def test_vii(self):
+        value = parse("VII")
+        assert value == 7
+
+    def test_viii(self):
+        value = parse("VIII")
+        assert value == 8
